27b6699779d6c687becfc8d3452a5574b740fe96
hogiahien <86543292+hogiahien@users.noreply.github.com>
2024-06-07 23:05:03 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ then
 fi
 
 printf "$vncpassword\n$vncpassword\n\n" | vncserver :1
-/.novnc/utils/launch.sh --vnc localhost:5901 --listen 8080
+/.novnc/utils/launch.sh --vnc localhost:5901 --listen 7860
